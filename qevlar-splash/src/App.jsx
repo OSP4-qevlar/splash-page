d4d204d1fc99c2ae076fec9ecadbdf13c84455b2
joshuamcdaniel95 <joshua.mcdaniel1995@gmail.com>
2023-12-18 13:01:35 -0700

    Working on Test Summary component.

--- qevlar-splash/src/App.jsx
+++ qevlar-splash/src/App.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import GitHubButton from 'react-github-btn';
 import NavBar from './components/NavBar'
 import Intro from './components/Intro';
+import TestSummary from './components/TestSummary'
 import './App.css'; // Your main CSS file that imports other styles
 
 const App = () => {
-  
+
   return (
     <div>
       <NavBar />
       <Intro />
       {/* ... other components go here ... */}
+      <TestSummary />
     </div>
   );
 }
--- qevlar-splash/src/components/NavBar.jsx
+++ qevlar-splash/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../src/App.css';
+// import '../../src/App.css';
 import GitHubButton from 'react-github-btn'
 
 const NavBar = () => {
@@ -13,9 +13,9 @@ const NavBar = () => {
           <a href="#devinfo">Developer Info</a>
         </div>
         <div className="nav-cta">
-            <button className="button-primary" href="">Developer Docs</button>
-            <GitHubButton className="button" href="https://github.com/oslabs-beta/qevlar/packages" data-color-scheme="no-preference: dark_high_contrast; light: light; dark: dark;" data-icon="octicon-package" data-size="large" aria-label="Install this package oslabs-beta/qevlar on GitHub">Install this package</GitHubButton>
-            <GitHubButton href="https://github.com/oslabs-beta/qevlar" data-color-scheme="no-preference: dark_high_contrast; light: light; dark: dark;" data-icon="octicon-star" data-size="large" aria-label="Star oslabs-beta/qevlar on GitHub">Star</GitHubButton>
+          <button className="button-primary" href="">Developer Docs</button>
+          <GitHubButton className="button" href="https://github.com/oslabs-beta/qevlar/packages" data-color-scheme="no-preference: dark_high_contrast; light: light; dark: dark;" data-icon="octicon-package" data-size="large" aria-label="Install this package oslabs-beta/qevlar on GitHub">Install this package</GitHubButton>
+          <GitHubButton href="https://github.com/oslabs-beta/qevlar" data-color-scheme="no-preference: dark_high_contrast; light: light; dark: dark;" data-icon="octicon-star" data-size="large" aria-label="Star oslabs-beta/qevlar on GitHub">Star</GitHubButton>
         </div>
       </nav>
     </header>
