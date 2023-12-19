3d1a1a554b68aff8dfa22190b0d9a0e961cd88fa
Landon Osteen <landonwyatteosteen@gmail.com>
2023-12-19 10:59:05 -0500

    finished mobile menu and navbar

--- qevlar-splash/src/components/NavBar.jsx
+++ qevlar-splash/src/components/NavBar.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
-import '../../src/App.css';
-import GitHubButton from 'react-github-btn'
+import React, { useState } from 'react';
+import { FaGithub, FaBars, FaTimes } from "react-icons/fa"; 
 
 const NavBar = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(prevState => !prevState);
+  };
+
   return (
     <header>
       <nav className="navbar">
         <div className="logo">qevlar</div>
-        <div className="nav-links">
-          <a href="#features">Features</a>
-          <a href="#teaminfo">Team Info</a>
-          <a href="#devinfo">Developer Info</a>
-        </div>
-        <div className="nav-cta">
-            <button className="button-primary" href="">Developer Docs</button>
-            <GitHubButton className="button" href="https://github.com/oslabs-beta/qevlar/packages" data-color-scheme="no-preference: dark_high_contrast; light: light; dark: dark;" data-icon="octicon-package" data-size="large" aria-label="Install this package oslabs-beta/qevlar on GitHub">Install this package</GitHubButton>
-            <GitHubButton href="https://github.com/oslabs-beta/qevlar" data-color-scheme="no-preference: dark_high_contrast; light: light; dark: dark;" data-icon="octicon-star" data-size="large" aria-label="Star oslabs-beta/qevlar on GitHub">Star</GitHubButton>
+        {/* Toggle button */}
+        <button className="hamburger" onClick={toggleMobileMenu} aria-label="Toggle navigation menu">
+          {isMobileMenuOpen ? <FaTimes /> : <FaBars />} {/* This line toggles the icon */}
+        </button>
+        <div className={`nav-links ${isMobileMenuOpen ? 'open' : ''}`}>
+          {/* Close mobile menu when a link is clicked */}
+          <a href="#features" onClick={toggleMobileMenu}>Features</a>
+          <a href="#teaminfo" onClick={toggleMobileMenu}>Team Info</a>
+          <a href="#devinfo" onClick={toggleMobileMenu}>Developer Info</a>
+          <button className="button-navigation" onClick={toggleMobileMenu}>
+            <FaGithub /> See Our Github
+          </button>
         </div>
       </nav>
+      {/* The overlay that appears when the menu is open */}
+      {isMobileMenuOpen && <div className="mobile-menu-overlay" onClick={toggleMobileMenu}></div>}
     </header>
   );
 }
