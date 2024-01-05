f8fc3c99aaeb7c53dfeead0b2001806da9296228
joshuamcdaniel95 <joshua.mcdaniel1995@gmail.com>
2024-01-05 13:09:33 -0700

    Configured Tailwind. Not able to see updates reflected in React components yet.

---
+++ qevlar-splash/src/components/TestExampleTabs.jsx
@@ -0,0 +1,14 @@
+import "../../src/App.css";
+import "../../src/index.css";
+import testcarbon from '../assets/testcarbon.png'
+
+const TestExampleTabs = () => {
+  return (
+    <section>
+      {/* <img src={testcarbon} className="text-green-50" /> */}
+      <h1 className="text-primary">This will be GREEN</h1>
+    </section>
+  )
+}
+
+export default TestExampleTabs;
---
+++ qevlar-splash/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#366B45"
+      }
+    },
+  },
+  plugins: [],
+}
+
